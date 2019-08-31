fc8e97c6f11d506a176213f565d35c2275b8dc81
SoonHyung_Lee <tnsgud9@naver.com>
2019-09-01 00:15:34 +0900

    default Form Update && SteamVR 1.2.3 version Add

---
+++ Assets/#Scripts/ControlManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.XR;
+using Valve.VR;
+
+
+public class ControlManager : MonoBehaviour
+{
+    public static SteamVR_TrackedObject controller;
+    public static SteamVR_Controller.Device device;
+
+    void Start()
+    {
+        controller = GetComponent<SteamVR_TrackedObject>();
+        device = GetComponent<SteamVR_Controller.Device>();
+    }
+
+    void Update()
+    {
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+
+        }
+    }
+}
